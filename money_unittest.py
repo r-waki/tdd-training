84b3888ed11cd089a62fca027d7bca4b12035909
r-waki <r.wakeyii@gmail.com>
2021-03-19 02:01:18 +0900

    clean : equality test

--- money_unittest.py
+++ money_unittest.py
@@ -8,19 +8,17 @@ class MoneyTest(unittest.TestCase) :
         self.assertEqual(Dollar(15).amount, five.times(3).amount)
     
     def testEquality(self) :
-        five = Dollar(5)
-        self.assertTrue(five.equals(Dollar(5)))
-        self.assertFalse(five.equals(Dollar(6)))
-    
+        dollar_five = Dollar(5)
+        franc_five = Franc(5)
+        self.assertTrue(dollar_five.equals(Dollar(5)))
+        self.assertFalse(dollar_five.equals(Dollar(6)))
+        self.assertTrue(franc_five.equals(Franc(5)))
+        self.assertFalse(franc_five.equals(Franc(4)))   
+        
     def testFrancMultiplication(self) :
         five = Franc(5)
         self.assertEqual(Franc(10).amount, five.times(2).amount) 
         self.assertEqual(Franc(15).amount, five.times(3).amount)
     
-    def testFrancEquality(self) :
-        five = Franc(5)
-        self.assertTrue(five.equals(Franc(5)))
-        self.assertFalse(five.equals(Franc(4)))
-
 if __name__ == '__main__':
     unittest.main()
