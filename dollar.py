d52040c1a8b1b1e0567c569e13bf3558b5fc1a33
r-waki <r.wakeyii@gmail.com>
2021-03-19 01:51:59 +0900

    clean : Dollar and Franc class, move init method to Money class

--- dollar.py
+++ dollar.py
@@ -1,17 +1,13 @@
+from money import Money
 
-class Dollar:
-    def __init__ (self, amount) :
-        self.amount = amount
-
+class Dollar(Money):
     def times(self, multiplier) :
         return Dollar(self.amount * multiplier)
     
     def equals(self, dollar) :
         return self.amount == dollar.amount
 
-class Franc:
-    def __init__ (self, amount) :
-        self.amount = amount
+class Franc(Money):
 
     def times(self, multiplier) :
         return Franc(self.amount * multiplier)
---
+++ money.py
@@ -0,0 +1,6 @@
+
+class Money :
+    def __init__ (self, amount) :
+        self.amount = amount
+
+
